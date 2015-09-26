69f55323707f152d45f847a6f292092eefde1fea
Alvin Wan <hi@alvinwan.com>
2015-09-26 11:02:52 -0700

    updated prod conf

--- outline_logic/config.py
+++ outline_logic/config.py
@@ -9,35 +9,39 @@ Various setups for different app instances
 
 
 class Config:
-	"""Default config"""
+    """Default config"""
 
-	DEBUG = False
-	TESTING = False
-	SESSION_STORE = 'session'
-	MONGODB_DB = 'default'
-	SECRET_KEY = 'flask+braiiin=<3'
-	LIVE = ['v1']
-	STATIC_PATH = 'static'
-	HASHING_ROUNDS = 15
+    DEBUG = False
+    TESTING = False
+    SESSION_STORE = 'session'
+    MONGODB_DB = 'default'
+    SECRET_KEY = 'flask+braiiin=<3'
+    LIVE = ['v1']
+    STATIC_PATH = 'static'
+    HASHING_ROUNDS = 15
 
-	INIT = {
-		'port': 8006,
-		'host': '127.0.0.1',
-	}
+    INIT = {
+        'port': 8006,
+        'host': '127.0.0.1',
+    }
 
 
 class ProductionConfig(Config):
-	"""Production vars"""
-	pass
+    """Production vars"""
+
+    INIT = {
+        'port': 80,
+        'host': '127.0.0.1',
+    }
 
 
 class DevelopmentConfig(Config):
-	"""For local runs"""
-	DEBUG = True
-	MONGODB_DB = 'dev'
+    """For local runs"""
+    DEBUG = True
+    MONGODB_DB = 'dev'
 
 
 class TestConfig(Config):
-	"""For automated testing"""
-	TESTING = True
-	MONGODB_DB = 'test'
+    """For automated testing"""
+    TESTING = True
+    MONGODB_DB = 'test'
